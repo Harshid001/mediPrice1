a407727a5fa650d6b273552da19f22fb37423cbf
Harshid001 <harshidsoni01@gmail.com>
2026-05-01 13:50:10 +0530

    App.jsx file has been added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+
+import Home from './pages/Home';
+import Compare from './pages/Compare';
+import GenericGuide from './pages/GenericGuide';
+import About from './pages/About';
+import SmartRxOptimizer from './pages/SmartRxOptimizer';
+
+import StoreLocator from './pages/StoreLocator';
+import ReportViolation from './pages/ReportViolation';
+import PriceAlert from './pages/PriceAlert';
+
+import AdminDashboard from './pages/AdminDashboard';
+import RegulatoryCompliance from './pages/RegulatoryCompliance';
+import Insights from './pages/Insights';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="compare" element={<Compare />} />
+        <Route path="guide" element={<GenericGuide />} />
+        <Route path="optimizer" element={<SmartRxOptimizer />} />
+        <Route path="about" element={<About />} />
+        <Route path="locator" element={<StoreLocator />} />
+        <Route path="report" element={<ReportViolation />} />
+        <Route path="alerts" element={<PriceAlert />} />
+        <Route path="admin" element={<AdminDashboard />} />
+        <Route path="compliance" element={<RegulatoryCompliance />} />
+        <Route path="insights" element={<Insights />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
